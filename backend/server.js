32f0cecd0482c2f6fa7c227d81ad53bee2c76d4c
dinoh <dino.himaj@student.tugraz.at>
2025-01-17 06:47:18 +0100

    established a DB connection with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,32 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 //app
 const app = express();
 
+//db
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log('DB connected successfully'))
+  .catch((err) => console.log('DB connection error:', err));
+
 //middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 //cors
-app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 //routes
 app.get('/api', (req, res) => {
   res.json({
-    data: 'hello from api',
+    data: 'hello from api test2',
     time: new Date().toLocaleString(),
   });
 });
