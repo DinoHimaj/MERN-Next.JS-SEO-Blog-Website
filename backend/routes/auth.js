1a2762dec05bd4afa65dcde577e127c444834f21
dinoh <dino.himaj@student.tugraz.at>
2025-01-21 06:40:50 +0100

    implemented Signup validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { signup } = require('../controllers/auth');
+//validators
+const { runValidation } = require('../validators');
+const { userSignupValidator } = require('../validators/auth');
 
-router.post('/signup', signup);
+console.log('signup:', signup);
+console.log('runValidation:', runValidation);
+console.log('userSignupValidator:', userSignupValidator);
+
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
